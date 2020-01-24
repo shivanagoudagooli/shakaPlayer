443184461396f4451d0a8a753272e46214f5005b
shivanagouagooli <shivanagouagooli@gmail.com>
2020-01-24 13:14:05 +0530

    second changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HelloWord from '../components/HelloWord.vue'
+import justPage from '../components/justPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'HelloWord',
+    component: HelloWord
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/justPage',
+    name:'justPage',
+    component:justPage
   }
 ]
 
