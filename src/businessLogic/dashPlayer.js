5c4b72d1619d0837cd0994485f820c598f0cb182
shivanagoudagooli <shivanagoudagooli@gmail.com>
2020-01-30 20:17:42 +0530

    first commit

--- src/businessLogic/dashPlayer.js
+++ src/businessLogic/dashPlayer.js
@@ -1,10 +1,20 @@
-// import vue from 'vue'
 import shaka from 'shaka-player'
-var manifestUri =
-    'https://preprod-cdn.cloud.altbalaji.com/content/2016-02/290-56c43f146a4e8/manifest.mpd';
+// var manifestUri =
+// "https://preprod-cdn.cloud.altbalaji.com/content/2016-02/290-56c43f146a4e8/manifest.mpd";
+var manifestUri = "https://cdn.cloud.altbalaji.com/content/2018-08/4779-5b6312848ae36/manifest.mpd";
+//"https://cdn.cloud.altbalaji.com/content/2018-08/4779-5b6312848ae36/manifest.mpd";
+
+    // 'https://preprod-cdn.cloud.altbalaji.com/content/2016-06/509-576c10ecd6cf7/manifest.mpd';
+    // 'https://storage.googleapis.com/shaka-demo-assets/sintel-widevine/dash.mpd';
+    var licenseServer = "https://uwkc9hqxld.execute-api.us-east-1.amazonaws.com/production/logthis";
+    // 'https://cwip-shaka-proxy.appspot.com/header_auth';
+    // var licenseServer =
+    // 'https://cwip-shaka-proxy.appspot.com/wrapped_request_and_response';
+    var authTokenServer = 'https://cwip-shaka-proxy.appspot.com/get_auth_token';
+    var authToken = null;
+
 
 function initApp() {
-  // Install built-in polyfills to patch browser incompatibilities.
   shaka.polyfill.installAll();
 
   // Check to see if the browser supports the basic APIs Shaka needs.
@@ -21,7 +31,6 @@ function initPlayer() {
   // Create a Player instance.
   var video = document.getElementById('video');
   var player = new shaka.Player(video);
-  console.log(player,"jhjhkjhhkjhjhkhkhjkhhk");
 
   // Attach player to the window to make it easy to access in the JS console.
   window.player = player;
@@ -29,14 +38,89 @@ function initPlayer() {
   // Listen for error events.
   player.addEventListener('error', onErrorEvent);
 
-  // Try to load a manifest.
-  // This is an asynchronous process.
+
+  player.configure('preferredAudioLanguage', 'fr-CA');
+// NOTE: language preferences affect the next call to load()
+
+// set text language preference to Greek and buffering goal to 2 minutes:
+player.configure({
+  streaming: {
+    bufferingGoal: 120,
+    rebufferingGoal: 15,
+    bufferBehind:30
+  }
+});
+player.configure({
+  manifest: {
+    retryParameters: {
+      timeout: 0,       // timeout in ms, after which we abort; 0 means never
+      maxAttempts: 2,   // the maximum number of requests before we fail
+      baseDelay: 1000,  // the base delay in ms between retries
+      backoffFactor: 2, // the multiplicative backoff factor between retries
+      fuzzFactor: 0.5,  // the fuzz factor to apply to each retry delay
+    }
+  }
+});
+player.configure({
+  drm: {
+    servers: {
+       'com.widevine.alpha': 'https://foo.bar/drm/widevine'
+      // 'com.microsoft.playready': 'https://foo.bar/drm/playready'
+    }
+  }
+});
+player.configure({
+  drm: {
+    clearKeys: {
+      'deadbeefdeadbeefdeadbeefdeadbeef': '18675309186753091867530918675309'
+      // '02030507011013017019023029031037': '03050701302303204201080425098033'
+    }
+  }
+});
+player.configure({
+  drm: {
+    servers: { 'com.widevine.alpha': licenseServer },
+    advanced: {
+      'com.widevine.alpha': {
+        'videoRobustness': 'HW_SECURE_ALL',
+        'audioRobustness': 'HW_SECURE_ALL'
+      }
+    }
+  }
+});
+player.getNetworkingEngine().registerRequestFilter(function(type, request) {
+  // Only add headers to license requests:
+  if (type != shaka.net.NetworkingEngine.RequestType.LICENSE) return;
+
+//   // If we already know the token, attach it right away:
+  if (authToken) {
+    console.log('Have auth token, attaching to license request.');
+    request.headers['CWIP-Auth-Header'] = authToken;
+    return;
+  }
+
+  console.log('Need auth token.');
+  // Start an asynchronous request, and return a Promise chain based on that.
+  var authRequest = {
+    uris: [authTokenServer],
+    method: 'POST',
+  };
+  var requestType = shaka.net.NetworkingEngine.RequestType.APP;
+  return player.getNetworkingEngine().request(requestType, authRequest)
+      .promise.then(function(response) {
+        // This endpoint responds with the value we should use in the header.
+        authToken = shaka.util.StringUtils.fromUTF8(response.data);
+        console.log('Received auth token', authToken);
+        request.headers['CWIP-Auth-Header'] = authToken;
+        console.log('License request can now continue.');
+      });
+  });
+
   player.load(manifestUri).then(function() {
     // This runs if the asynchronous load is successful.
     console.log('The video has now been loaded!');
   }).catch(onError);  // onError is executed if the asynchronous load fails.
 }
-
 function onErrorEvent(event) {
   // Extract the shaka.util.Error object from the event.
   onError(event.detail);
--- src/components/HelloWord.vue
+++ src/components/HelloWord.vue
@@ -1,43 +1,161 @@
 <template>
-  <div>
-      <div>
-          <img src="../assets/play-button.png" id="img" v-on:click="play">
+ <div>
+    <div>
+        <div v-bind:class="[display ? 'img': 'eimg']">
+        <img v-show="display1" src="../assets/play-button.png" class="video" @click="playVd()" >
+        <img v-if="!display1" src="../assets/pause.png" class="video" @click="playVd()">
+        <img src="../assets/rewind.png" class="rewind" @click="rewind()" >
+        <img src="../assets/fast-forward.png" class="forward" @click="forward()">
       </div>
-        <video id="video" class="video"
-           width="640px"
-           poster="//shaka-player-demo.appspot.com/assets/poster.jpg"
-           controls
-           ></video>
+        <img v-show="display" src="../assets/expand.png" class="expand" @click="expand()">
+        <img v-if="!display"  src="../assets/shrink.png" class="shrink" @click="makeSmall()">
+      <div style="margin-left:25px;">
+      <video id="video" muted="muted" width="664px" style=margin-left:35px;margin-top:0px;>
+      </video><br>
+      <input id="seekslider" type="range" min="0" max="100" value="0" step="1"  @click="initializsPlayer()" :class="[display ? 'seekbar':'seekbar1']">
+      </div>
+    </div>
   </div>
-
 </template>
-
 <script>
 import * as func from '../businessLogic/dashPlayer';
 
-export default{
-created(){
-},
-mounted(){
-},
-methods:{
-    play(){
-        // this.$router.push('justPage');
-        func.initPlayer;
+export default {   
+  data () {
+    return{
+     display:true,
+     display1:true,
+     display2:true
     }
-}
-}
+  },
+  created(){
+  },
+  methods:{
+      
+      playVd(){
+        let myVideo = document.getElementById("video");
+        console.log(myVideo);
+        if(func.initPlayer){
+            
+           this.display1 = false;
+          myVideo.play();
+        }
+        else{
+          myVideo.pause();
+          this.display1 = true;
+        }
+      },
+      expand(){
+        this.display =false
+        let myVideo = document.getElementById("video");
+        myVideo.width = 1200;
+      },
+      makeSmall(){
+        let myVideo = document.getElementById("video");
+        this.display = true;
+        myVideo.width = 664;
+      },
+    
+      initializsPlayer(){
+        let seekslider = document.getElementById("seekslider");
+      let vid = document.getElementById("video");
+        seekslider.addEventListener("change",this.vidseek(),false)
+        vid.addEventListener("timeupdate",this.seektimeupdate(),false)
+      },
+      vidseek(){
+        let seekslider = document.getElementById("seekslider");
+        let vid = document.getElementById("video");
+        let seekto = vid.duration * (seekslider.value/100);
+      
+        vid.currentTime =seekto;
+        console.log(seekto,"end")
+      },
+      seektimeupdate(){
+        var seekslider = document.getElementById("seekslider");
+        var vid = document.getElementById("video");
+  
+        let seekto = vid.duration * (seekslider.value/100);
+        vid.currentTime =seekto;
 
+        var nt = vid.currentTime * (100 / vid.duration);
+        seekslider.value = nt;
+      },
+      rewind(){
+          console.log("rewind")
+          let seekslider = document.getElementById("seekslider");
+        let vid = document.getElementById("video");
+        let time = vid.duration * (seekslider.value/100);
+        vid.currentTime =time;
+        
+        let nt = vid.currentTime * (100/vid.duration);
+        seekslider.value= nt-10;
+        console.log("rewind end")
+      },
+      forward(){
+          console.log("forward")
+          let seekslider = document.getElementById("seekslider");
+        let vid = document.getElementById("video");
+        let time = vid.duration * (seekslider.value/100);
+        vid.currentTime =time;
+        
+        let nt = vid.currentTime * (100/vid.duration);
+        seekslider.value= nt+10;
+      }
+        
+     
+    
+  }
+}
 </script>
 
-<style>
-#img{
-    top:22%;
-    position:absolute;
-    margin-inline-start: 279px;
-}
+<style  scoped>
 .video{
-    opacity:1;
+  position:absolute;
+  margin-inline-start: 289px;
+  z-index: 1;
+}
+.img{
+  top:23%;
+  padding-left: 50px;
+  margin-left: 25px;
+  position:absolute;
+  
+}
+.eimg{
+    text-align: center;
+    top:51%;
+    padding-left: 322px;
+    position: absolute;
+}
+.expand{
+    top:45%;
+  position:absolute;
+  margin-left: 673px;
+  z-index: 1;
+  
+}
+.shrink{
+    padding-top: 44%;
+    position: absolute;
+    z-index: 1;
+    padding-left: 86%;
+
+}
+.seekbar{
+  width: 686px;
+}
+.seekbar1{
+  width:1237px;
+}
+.rewind{
+  position:absolute;
+  margin-left: 143px;
+  z-index: 1;
+}
+.forward{
+  position:absolute;
+  margin-left: 433px;
+  z-index: 1; 
 }
 
 </style>
